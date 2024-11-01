81d3b98dd1b254cb9ef6079820c853beb1874cca
Mohamed-Hmidouch <ffa54202@gmail.com>
2024-11-01 15:04:41 +0100

    push_ex

--- invoice.js
+++ invoice.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    // add initital total calculation first 
-        //here
-    
-    // these are selection helpers
-    const invoiceTable = document.getElementById('invoiceTable').getElementsByTagName('tbody')[0];
+    const invoiceTable = document.getElementById('invoiceTable');
     const invoiceTotal = document.getElementById('invoiceTotal');
+    
+    // add initital total calculation first 
+     document.getElementsByTagName('td')[1].getElementsByClassName('unitPrice');
+    let total_item = document.getElementsByTagName('td')[1].getElementsByClassName('quantity');
+        //here
+    let total_elemnts = document.getElementsByClassName('itemTotal');
+    // these are selection helpers
 
+    
 
+  let traduit = {
+    total_1 : total_elemnts.length[1],
+    total_2 : total_elemnts.length[2],
+    total_3 : total_elemnts.length[3]
+  };
+  let js = JSON.parse(traduit);
+    console.log(js);
 
     // This function updates the total amount in each row ( item price * Qte )
     function updateItemTotal(row) {
